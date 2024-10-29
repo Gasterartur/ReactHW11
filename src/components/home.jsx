9be0dd792081cb16c05229fb18f9ef56d349986d
Artur Gaster <gasterartur@googlemail.com>
2024-10-29 18:43:12 +0100

    First commit

---
+++ src/components/home.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Добро пожаловать на главную страницу блога!</h1>
+      <p>Здесь вы найдете последние статьи.</p>
+    </div>
+  );
+};
+
+export default Home;
